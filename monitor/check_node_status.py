19e0806a058d59544672db6619e580deda64f07f
calvin_chen <calvin_chen@trendmicro.com.cn>
2016-09-18 10:14:48 +0800

    change check_node_status code ,no mail bomb

--- monitor/check_node_status.py
+++ monitor/check_node_status.py
@@ -9,18 +9,49 @@ logging.basicConfig(filename='./run.log',
                     level=logging.DEBUG, filemode='a',
                     format='%(asctime)s - %(levelname)s: %(message)s')
 
+
 def check_node_status():
     try:
         b = commands.getoutput(PATH)
         logging.info(b)
         if "DN" in b or "Status=Up/Down" not in b:
-            sendmail.send_mail('10.204.16.7', 'node DOWN report', 'c* cluster', 'calvin_chen@trendmicro.com.cn', b)
+            if read_flag() == 'normal':
+                write_flag('abnormal')
+                send_mail_tool(b)
+            elif read_flag() == 'abnormal':
+                pass
+            else:
+                write_flag('abnormal')
+                send_mail_tool(b)
+
+        else:
+            write_flag('normal')
+
     except:
-        sendmail.send_mail('10.204.16.7', 'node DOWN report-ERROR', 'c* cluster', 'calvin_chen@trendmicro.com.cn', "")
-        logging.info("error occur!")
+        send_mail_tool("Check node status error!")
+
+
+def write_flag(flag):
+    with open('status.flag', 'w') as f:
+        f.write(flag)
+
+
+def read_flag():
+    if os.path.isfile('status.flag'):
+        with open('status.flag', 'b') as f:
+            flag_str = f.readline()
+            if 'abnormal' in flag_str:
+                return 'abnormal'
+            else:
+                return 'normal'
+    else:
+        return 'normal'
+
+
+def send_mail_tool(b):
+    logging.info("sendmail")
+    sendmail.send_mail('10.204.16.7', 'node DOWN report', 'c* cluster', 'calvin_chen@trendmicro.com.cn', b)
+    sendmail.send_mail('10.204.16.7', 'node DOWN report', 'c* cluster', 'wenjun_yang@trendmicro.com.cn', b)
 
 
 check_node_status()
-
-
-
